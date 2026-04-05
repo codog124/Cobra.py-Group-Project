262fedc89390de22699496cd10289015bcfde685
Will Rodgers <109996207+WillR1120@users.noreply.github.com>
2026-04-05 17:32:12 -0400

    Create promotions.py

---
+++ api/models/promotions.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime
+from ..dependencies.database import Base
+
+class Promotion(Base):
+    __tablename__ = "promotions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    code = Column(String, unique=True)
+    discount_percent = Column(Float)
+    expiration_date = Column(DateTime)
+    active = Column(Boolean, default=True)
