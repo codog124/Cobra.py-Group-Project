752044a42af3ce82c075de67ac58f642cc0d9c9a
Will Rodgers <109996207+WillR1120@users.noreply.github.com>
2026-04-05 17:30:41 -0400

    Update and rename order_details.py to order_items.py

---
+++ api/models/order_items.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from ..dependencies.database import Base
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    order_id = Column(Integer, ForeignKey("orders.id"))
+    menu_item_id = Column(Integer, ForeignKey("menu_items.id"))
+
+    quantity = Column(Integer, nullable=False)
+    price = Column(Float, nullable=False)
+
+    order = relationship("Order", back_populates="items")
+    menu_item = relationship("MenuItem", back_populates="order_items")
