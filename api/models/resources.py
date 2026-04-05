7fc3d600c040a8956341c8ea1e41fd4cd1f6a397
Will Rodgers <109996207+WillR1120@users.noreply.github.com>
2026-04-05 17:31:53 -0400

    Update resources.py

--- api/models/resources.py
+++ api/models/resources.py
@@ -1,14 +1,10 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
-from sqlalchemy.orm import relationship
-from datetime import datetime
+from sqlalchemy import Column, Integer, String, Float
 from ..dependencies.database import Base
-
 
 class Resource(Base):
     __tablename__ = "resources"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    item = Column(String(100), unique=True, nullable=False)
-    amount = Column(Integer, index=True, nullable=False, server_default='0.0')
-
-    recipes = relationship("Recipe", back_populates="resource")
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    quantity = Column(Float)
+    unit = Column(String)
