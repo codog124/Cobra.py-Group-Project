69d7ca64281e5ec8ff3e0cd59785f7072282b419
Will Rodgers <109996207+WillR1120@users.noreply.github.com>
2026-04-05 17:31:34 -0400

    Create reviews.py

---
+++ api/models/reviews.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from ..dependencies.database import Base
+
+class Review(Base):
+    __tablename__ = "reviews"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    menu_item_id = Column(Integer, ForeignKey("menu_items.id"))
+
+    rating = Column(Integer)
+    comment = Column(String)
+
+    user = relationship("User", back_populates="reviews")
+    menu_item = relationship("MenuItem", back_populates="reviews")
