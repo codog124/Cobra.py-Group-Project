499a072229ad5c4a92117dd3e405e097e5a778f8
ndmerritt <nathandmerritt@gmail.com>
2026-05-03 21:01:14 -0400

    password reset

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -3,6 +3,6 @@ class conf:
     db_name = "sandwich_maker_api"
     db_port = 3306
     db_user = "root"
-    db_password = "9197970107NdM"
+    db_password = "rootroot"
     app_host = "localhost"
     app_port = 8000
